23faf57dc4a973aa5c983e4f10024aaf2a534bff
Your Name <NRankapole@dvtsoftware.com>
2026-02-21 20:32:00 +0200

    Add basic folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
 
 function App() {
-  return <>
-  </> 
+  return (
+  <h1>Report Card Grader</h1>
+  )
 }
 
 export default App
---
+++ src/data/students.ts
@@ -0,0 +1,22 @@
+export const students = [
+  { id: 1, name: "Liam Johnson", mark: 92 },  
+  { id: 2, name: "Emma Williams", mark: 85 }, 
+  { id: 3, name: "Noah Brown", mark: 78 },    
+  { id: 4, name: "Olivia Jones", mark: 73 },  
+  { id: 5, name: "William Garcia", mark: 67 },
+  { id: 6, name: "Ava Miller", mark: 62 },    
+  { id: 7, name: "James Davis", mark: 58 },   
+  { id: 8, name: "Sophia Rodriguez", mark: 54 }, 
+  { id: 9, name: "Benjamin Martinez", mark: 46 }, 
+  { id: 10, name: "Isabella Hernandez", mark: 39 }, 
+  { id: 11, name: "Lucas Lopez", mark: 81 },   
+  { id: 12, name: "Mia Gonzalez", mark: 74 }, 
+  { id: 13, name: "Henry Wilson", mark: 69 }, 
+  { id: 14, name: "Charlotte Anderson", mark: 95 }, 
+  { id: 15, name: "Alexander Thomas", mark: 71 }, 
+  { id: 16, name: "Amelia Taylor", mark: 60 }, 
+  { id: 17, name: "Daniel Moore", mark: 57 }, 
+  { id: 18, name: "Harper Jackson", mark: 83 },  
+  { id: 19, name: "Matthew Martin", mark: 52 }, 
+  { id: 20, name: "Evelyn Lee", mark: 88 }
+];
