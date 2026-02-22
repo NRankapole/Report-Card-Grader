a46dc30758ac0833233cc2f2e568ace849fda735
Your Name <NRankapole@dvtsoftware.com>
2026-02-22 16:06:45 +0200

    feat: Add grade property to student object

--- src/data/students.ts
+++ src/data/students.ts
@@ -1,28 +1,29 @@
 export interface StudentProp{
   id: number,
   name: string,
-  mark: number
+  mark: number,
+  grade: string
 };
 
 export const students: StudentProp[] = [
-  { id: 1, name: "Liam Johnson", mark: 92 },  
-  { id: 2, name: "Emma Williams", mark: 85 }, 
-  { id: 3, name: "Noah Brown", mark: 78 },    
-  { id: 4, name: "Olivia Jones", mark: 73 },  
-  { id: 5, name: "William Garcia", mark: 67 },
-  { id: 6, name: "Ava Miller", mark: 62 },    
-  { id: 7, name: "James Davis", mark: 58 },   
-  { id: 8, name: "Sophia Rodriguez", mark: 54 }, 
-  { id: 9, name: "Benjamin Martinez", mark: 46 }, 
-  { id: 10, name: "Isabella Hernandez", mark: 39 }, 
-  { id: 11, name: "Lucas Lopez", mark: 81 },   
-  { id: 12, name: "Mia Gonzalez", mark: 74 }, 
-  { id: 13, name: "Henry Wilson", mark: 69 }, 
-  { id: 14, name: "Charlotte Anderson", mark: 95 }, 
-  { id: 15, name: "Alexander Thomas", mark: 71 }, 
-  { id: 16, name: "Amelia Taylor", mark: 60 }, 
-  { id: 17, name: "Daniel Moore", mark: 57 }, 
-  { id: 18, name: "Harper Jackson", mark: 83 },  
-  { id: 19, name: "Matthew Martin", mark: 52 }, 
-  { id: 20, name: "Evelyn Lee", mark: 88 }
+  { id: 1, name: "Liam Johnson", mark: 92 , grade: ""},  
+  { id: 2, name: "Emma Williams", mark: 85 , grade: ""}, 
+  { id: 3, name: "Noah Brown", mark: 78 , grade: ""},    
+  { id: 4, name: "Olivia Jones", mark: 73 , grade: ""},  
+  { id: 5, name: "William Garcia", mark: 67 , grade: ""},
+  { id: 6, name: "Ava Miller", mark: 62 , grade: ""},    
+  { id: 7, name: "James Davis", mark: 58 , grade: ""},   
+  { id: 8, name: "Sophia Rodriguez", mark: 54 , grade: ""}, 
+  { id: 9, name: "Benjamin Martinez", mark: 46 , grade: ""}, 
+  { id: 10, name: "Isabella Hernandez", mark: 39 , grade: ""}, 
+  { id: 11, name: "Lucas Lopez", mark: 81 , grade: ""},   
+  { id: 12, name: "Mia Gonzalez", mark: 74 , grade: ""}, 
+  { id: 13, name: "Henry Wilson", mark: 69 , grade: ""}, 
+  { id: 14, name: "Charlotte Anderson", mark: 95, grade: "" }, 
+  { id: 15, name: "Alexander Thomas", mark: 71 , grade: ""}, 
+  { id: 16, name: "Amelia Taylor", mark: 60 , grade: ""}, 
+  { id: 17, name: "Daniel Moore", mark: 57 , grade: ""}, 
+  { id: 18, name: "Harper Jackson", mark: 83 , grade: ""},  
+  { id: 19, name: "Matthew Martin", mark: 52 , grade: ""}, 
+  { id: 20, name: "Evelyn Lee", mark: 88 , grade: ""}
 ];
